b8073712b17fe7d60e5da87419ea4693fac13745
Software Dude <SftwDude@outlook.com>
2017-08-14 14:24:36 -0400

    type:feat began to add Search

---
+++ src/ListSearchBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ListSearchBooks extends Component {
+  render() {
+    return (
+      <div className="list-SearchBooks">
+      </div>
+    )
+  }
+}
+
+ListSearchBooks.protoTypes = {
+}
+
+export default ListSearchBooks
